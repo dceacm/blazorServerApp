f90f7b02312893f2d789ee0172dc5f3f422d7233
d636066 <diego.cena.calvo@navarra.es>
2024-06-12 19:27:19 +0200

    Refactor general - Muevo usings a _Imports.razor

--- BlazorServerApp/Program.cs
+++ BlazorServerApp/Program.cs
@@ -1,31 +1,26 @@
 using BlazorServerApp;
 using BlazorServerApp.OpenAPIs;
 
+// Services
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddHttpContextAccessor();
-
-// API
 builder.Services.AddCustomHttpClient<IApiClient, ApiClientCustom>();
 
+// App
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
